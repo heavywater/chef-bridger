6e69a52ab930c56bbe73985f829b5fb757b885ad
Chris Roberts <chrisroberts.code@gmail.com>
2012-09-18 13:42:45 -0700

    Add relocation message

--- metadata.rb
+++
@@ -1,9 +0,0 @@
-maintainer       "Chris Roberts"
-maintainer_email "chrisroberts.code@gmail.com"
-license          "Apache 2.0"
-description      "Create bridges"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
-
-supports 'ubuntu', '12.04'
-
